cb653e9010648e02962631e5828285634d335893
flokoe <koehler.florian@gmx.de>
2023-07-23 20:05:31 +0200

    Add new design

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,17 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
+const purgecss = { '@fullhuman/postcss-purgecss': {
   content: [ './hugo_stats.json' ],
   defaultExtractor: (content) => {
       let els = JSON.parse(content).htmlElements;
       return els.tags.concat(els.classes, els.ids);
   }
-})
+}
+}
 
 module.exports = {
   plugins: {
-    "postcss-import": {},
+    'postcss-import': {},
     autoprefixer: {},
-    ...(process.env.HUGO_ENVIRONMENT === 'production' ? { purgecss } : {})
+    ...(process.env.HUGO_ENVIRONMENT === 'production' ? purgecss : {}),
+    ...(process.env.HUGO_ENVIRONMENT === 'production' ? { cssnano: { preset: 'advanced' } } : {})
   }
 }
